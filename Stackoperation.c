ae5c5dc34bc9d8e746e81a4e6773f58b4d8fb487
Arpitha JK <150835585+ArpithaJK4@users.noreply.github.com>
2024-03-05 09:26:02 -0800

    Create Stackoperation.c

---
+++ Stackoperation.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SIZE 5
+
+
+
+void push( int *, int);
+void pop(int*);
+
+void display(int*);
+int top=-1;
+void main(){
+int stack[SIZE];
+int choice , item;
+while(1){
+printf("\n\nStack operation:");
+printf("\n 1 push");
+printf("\n 2 pop");
+printf("\n 3 Display");
+
+printf("\n 4 Exit");
+
+
+
+
+
+
+
+}
+
+
+}
